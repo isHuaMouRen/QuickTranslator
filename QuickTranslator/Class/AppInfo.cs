8023842f37ae4771503466dbf5e3cde42b4b19c9
HuaMouRen <2825338723@qq.com>
2025-11-30 18:18:57 +0800

    feat(app-info): 添加AppInfo全局类

---
+++ QuickTranslator/Class/AppInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace QuickTranslator.Class
+{
+    public static class AppInfo
+    {
+        //字符串
+        public static readonly string Version = "1.0.0-alpha.1";
+
+        //执行目录
+        public static readonly string ExecuteDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)!;
+
+        //URL
+        public static readonly string ApiUrl = "https://60api.09cdn.xyz/v2/fanyi";
+    }
+}
