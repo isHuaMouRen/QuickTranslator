177283e6c0938721a81277cd6727dcd9e4f6b6af
HuaMouRen <2825338723@qq.com>
2025-11-30 18:40:00 +0800

    feat(json-language-list): 添加语言列表Json类

---
+++ QuickTranslator/Class/JsonConfigs/JsonLanguageList.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace QuickTranslator.Class.JsonConfigs
+{
+    public class JsonLanguageList
+    {
+        public class Index
+        {
+            [JsonProperty("code")]
+            public int Code { get; set; }
+
+            [JsonProperty("message")]
+            public string Message { get; set; }
+
+            [JsonProperty("data")]
+            public LanguageInfo[] Data {  get; set; }
+        }
+
+        public class LanguageInfo
+        {
+            [JsonProperty("code")]
+            public string Code { get; set; }
+
+            [JsonProperty("label")]
+            public string Label { get; set; }
+
+            [JsonProperty("alphabet")]
+            public string Alphabet { get; set; }
+        }
+    }
+}
