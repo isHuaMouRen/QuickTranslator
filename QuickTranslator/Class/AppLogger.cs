e0bc86f721b902bf307931c4d7f9546ef74452a3
HuaMouRen <2825338723@qq.com>
2025-11-30 18:26:44 +0800

    feat(app-logger): 添加 `AppLogger` 全局类

---
+++ QuickTranslator/Class/AppLogger.cs
@@ -0,0 +1,18 @@
+﻿using HuaZi.Library.Logger;
+using System.IO;
+using System.Text;
+
+namespace QuickTranslator.Class
+{
+    public static class AppLogger
+    {
+        public static Logger logger = new Logger
+        {
+            LogDirectory = Path.Combine(AppInfo.ExecuteDirectory, "Logs"),
+            ShowCallerInfo = false,
+            ShowDate = false,
+            Encoding = Encoding.UTF8,
+            ShowTime = true
+        };
+    }
+}
