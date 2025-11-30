291d61b3531b87c5160099e1800182a2a1cc501b
HuaMouRen <2825338723@qq.com>
2025-11-30 19:48:28 +0800

    feat(app-info): 添加配置变量

--- QuickTranslator/Class/AppInfo.cs
+++ QuickTranslator/Class/AppInfo.cs
@@ -1,17 +1,20 @@
-﻿using System.IO;
+﻿using QuickTranslator.Class.JsonConfigs;
+using System.IO;
 using System.Reflection;
 
 namespace QuickTranslator.Class
 {
     public static class AppInfo
     {
-        //字符串
+        //版本
         public static readonly string Version = "1.0.0-alpha.1";
-
         //执行目录
         public static readonly string ExecuteDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)!;
-
-        //URL
+        //API URL
         public static readonly string ApiUrl = "https://60api.09cdn.xyz/v2/fanyi";
+        //配置
+        public static JsonAppConfig.Index Config;
+        //配置文件路径
+        public static readonly string ConfigPath = Path.Combine(ExecuteDirectory, "config.json");
     }
 }
