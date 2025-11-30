47c5458f865c5581587c0fbd47f974a9da496474
HuaMouRen <2825338723@qq.com>
2025-11-30 18:31:35 +0800

    feat(json-api): 添加 Api Json类

---
+++ QuickTranslator/Class/JsonConfigs/JsonApi.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace QuickTranslator.Class.JsonConfigs
+{
+    public class JsonApi
+    {
+        public class Index
+        {
+            [JsonProperty("code")]
+            public int Code { get; set; }
+
+            [JsonProperty("message")]
+            public string Message { get; set; }
+
+            [JsonProperty("data")]
+            public JsonApi.Data Data { get; set; }
+        }
+
+        public class Data
+        {
+            [JsonProperty("source")]
+            public JsonApi.LanguageInfo Source { get; set; }
+
+            [JsonProperty("target")]
+            public JsonApi.LanguageInfo Target { get; set; }
+        }
+
+        public class LanguageInfo
+        {
+            [JsonProperty("text")]
+            public string Text { get; set; }
+
+            [JsonProperty("type")]
+            public string Type { get; set; }
+
+            [JsonProperty("type_desc")]
+            public string TypeDescription { get; set; }
+
+            [JsonProperty("pronounce")]
+            public string Pronounce { get; set; }
+        }
+    }
+}
