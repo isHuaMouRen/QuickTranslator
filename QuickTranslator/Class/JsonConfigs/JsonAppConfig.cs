33de4d15d06dd3e958dcdac7d86f88497392d051
HuaMouRen <2825338723@qq.com>
2025-11-30 19:03:44 +0800

    feat(json-app-config): 添加程序配置Json类

---
+++ QuickTranslator/Class/JsonConfigs/JsonAppConfig.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace QuickTranslator.Class.JsonConfigs
+{
+    public class JsonAppConfig
+    {
+        public class Index
+        {
+            [JsonProperty("source_language")]
+            public string SourceLanguage { get; set; }
+
+            [JsonProperty("target_language")]
+            public string TargetLanguage { get; set; }
+        }
+    }
+}
