939086e8a6c10380c555ff2aabbc51ade4235089
HuaMouRen <2825338723@qq.com>
2025-11-30 18:45:08 +0800

    feat(utils): 添加两个全局工具类

---
+++ QuickTranslator/Utils/DialogManager.cs
@@ -0,0 +1,50 @@
+﻿using ModernWpf.Controls;
+using static QuickTranslator.Class.AppLogger;
+
+namespace QuickTranslator.Utils
+{
+    public static class DialogManager
+    {
+        private static bool isDialogShow = false;
+
+        public static async Task ShowDialogAsync(ContentDialog dialog, Action? primaryCallback = null, Action? secondaryCallback = null, Action? closeCallback = null)
+        {
+            logger.Info($"[对话框管理器] 显示对话框: Title=\"{dialog.Title}\" Content=\"{dialog.Content}\" PrimaryButton=\"{dialog.PrimaryButtonText}\" SecondaryButton=\"{dialog.SecondaryButtonText}\" CloseButton=\"{dialog.CloseButtonText}\"");
+            logger.Info($"[对话框管理器] 等待对话框空闲");
+            // 等待直到没有对话框显示
+            await WaitForDialogToCloseAsync();
+            logger.Info($"[对话框管理器] 显示对话框: {dialog.Title}");
+            isDialogShow = true;
+
+            var result = await dialog.ShowAsync();
+            logger.Info($"[对话框管理器] 对话框关闭, 用户选择: {(
+                result == ContentDialogResult.Primary ? "Primary" :
+                result == ContentDialogResult.Secondary ? "Secondary" : "Close"
+            )}: {(
+                result == ContentDialogResult.Primary ? dialog.PrimaryButtonText :
+                result == ContentDialogResult.Secondary ? dialog.SecondaryButtonText : dialog.CloseButtonText
+            )}");
+
+            isDialogShow = false;
+            HandleDialogResult(result, primaryCallback, secondaryCallback, closeCallback);
+        }
+
+        private static async Task WaitForDialogToCloseAsync()
+        {
+            while (isDialogShow)
+            {
+                await Task.Delay(100);
+            }
+        }
+
+        private static void HandleDialogResult(ContentDialogResult result, Action? primaryCallback, Action? secondaryCallback = null, Action? closeCallback = null)
+        {
+            if (result == ContentDialogResult.Primary)
+                primaryCallback?.Invoke();
+            else if (result == ContentDialogResult.Secondary)
+                secondaryCallback?.Invoke();
+            else
+                closeCallback?.Invoke();
+        }
+    }
+}
---
+++ QuickTranslator/Utils/ErrorReportDialog.cs
@@ -0,0 +1,47 @@
+﻿using HuaZi.Library.Logger;
+using ModernWpf.Controls;
+using System.Windows.Controls;
+using System.Windows.Media;
+using static QuickTranslator.Class.AppLogger;
+
+namespace QuickTranslator.Utils
+{
+    public static class ErrorReportDialog
+    {
+        public static async void Show(string title, string message, Exception ex)
+        {
+            logger.Error($"\"{title}\": \"{message}\" => \n{ex}");
+
+            var dialog = new ContentDialog
+            {
+                Title = title,
+                Content = new StackPanel
+                {
+                    Children =
+                    {
+                        new System.Windows.Controls.ProgressBar
+                        {
+                            Value=100,
+                            Margin=new System.Windows.Thickness(0,0,0,10),
+                            Foreground=new SolidColorBrush(Color.FromRgb(255,50,50))
+                        },
+                        new Label
+                        {
+                            Content=message,
+                            Margin=new System.Windows.Thickness(0,0,0,10)
+                        },
+                        new TextBox
+                        {
+                            Text=ex.ToString(),
+                            IsReadOnly=true
+                        }
+                    }
+                },
+                PrimaryButtonText = "继续",
+                SecondaryButtonText = "终止",
+                DefaultButton = ContentDialogButton.Primary
+            };
+            await DialogManager.ShowDialogAsync(dialog, null, (() => Environment.Exit(1)));
+        }
+    }
+}
